5505ce85d9d5b6d849c313a4029fe9f2696dbaef
Ariana2203 <65371677+Ariana2203@users.noreply.github.com>
2020-05-30 17:26:37 -0600

    Update index.js

--- index.js
+++ index.js
@@ -1,55 +1,54 @@
-//Importamos los paquetes que requerimos
-const express = require('express');
-const hbs = require('hbs');
-
-//creamos la aplicacion
-const app = express();
-
-//Definimos el motor de plantillas
-//No de vamos a emplear MVC (Vista vamos a utiisar HBS)
+//Importamos paquetes requeridos
+const express=require('express');
+const hbs=require('hbs');
+//Creamos la aplicacion
+const app=express();
+//definimos el motor de plantillas
 app.set('view engine', 'hbs');
 
-//Vamos a generar los partials
-hbs.registerPartials(__dirname + '/views/partials');
-
-//Generamos el sitio estatico
 app.use(express.static(__dirname + '/public'));
+//Configurar rutas
+app.get('/',(req, res)=>{
+    res.render('index',{
+        autor: 'Ariana Piñon',
+        year: new Date().getFullYear(),
+        title: 'Inicio'
 
-//configurar mis rutas
-app.get('/', (req, res)=>{
-    res.render('index', {
-        autor : 'Raúl Toledo',
-        year : new Date().getFullYear(),
-        title : 'Inicio'
+    });
+    
+});
+app.get('/kahlo',(req, res)=>{
+    res.render('frida',{
+        autor: 'Ariana Piñon',
+        year: new Date().getFullYear(),
+        title: 'Frida Kahlo'
+    });
+});
+app.get('/warhol',(req, res)=>{
+    res.render('andy',{
+        autor: 'Ariana Piñon',
+        year: new Date().getFullYear(),
+        title: 'Salvador Dalí'
+    });
+});
+app.get('/velazquez',(req, res)=>{
+    res.render('diego',{
+        autor: 'Ariana Piñon',
+        year: new Date().getFullYear(),
+        title: 'Diego Velazquez'
+    });
+});
+app.get('/rivera',(req, res)=>{
+    res.render('diegoR',{
+        autor: 'Ariana Piñon',
+        year: new Date().getFullYear(),
+        title: 'Diego Rivera'
     });
 });
 
-app.get('/vangogh', (req, res)=>{
-    res.render('vincent', {
-        autor : 'Yo mero y que',
-        year : new Date().getFullYear(),
-        title : 'Vincent Van Gogh'
-    });
-});
-
-app.get('/salvador', (req, res)=>{
-    res.render('dali', {
-        autor : 'Lord Voldemort',
-        year : new Date().getFullYear(),
-        title : 'Salvador Dalí'
-    });
-});
-
-app.get('/kand', (req, res)=>{
-    res.render('kandisky', {
-        autor : 'Gandalf el gris',
-        year : new Date().getFullYear(),
-        title : 'Vasili Kandinski',
-        algo : "No se que poner"
-    });
-});
-
-//Arrancamos el servisdor web
+//Vamos a generar Partials
+hbs.registerPartials(__dirname + '/views/partials')
+//Servidor web
 app.listen(3000, ()=>{
-    console.log('Escuchando el puerto 3000');
+    console.log('Escuchando puerto 3000');
 });
